5bcf9c10c73e79274e63119724c0e51a1a4ec25f
Pere Pau Carrasco Quesada <perepaucarrasco@gmail.com>
2025-04-10 11:08:09 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-import "bootstrap";
-import "./style.css";
+
+const suits = ["♦",  "♥",  "♠",  "♣"]
+const value = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function getRandomCard () {
+    const randomSuits = suits[Math.floor(Math.random() * suits.length)];
+    const randomValues = value[Math.floor(Math.random() * value.length)];
+    return {suit: randomSuits, value: randomValues};
 };
+
+function updateCard () {
+    const {suit, value} = getRandomCard();
+
+
+    const topLeftSuit = document.querySelector(".top-left-suit")
+    const rightBottomSuit = document.querySelector(".bottom-right-suit")
+    const cardValue = document.querySelector(".card-value")
+
+    topLeftSuit.textContent= suit
+    rightBottomSuit.textContent= suit
+    cardValue.textContent=value
+
+    const isRed = (suit === "♥" || suit === "♦")
+    const color = isRed ? "red" : "black" 
+
+    topLeftSuit.style.color= color
+    rightBottomSuit.style.color= color
+    cardValue.style.color= color
+};
+
+window.onload = () => {
+    updateCard();
+    
+    document.getElementById("update-button").addEventListener("click", updateCard);
+}
+
